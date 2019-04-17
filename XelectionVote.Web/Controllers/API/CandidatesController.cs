ddf67c38d43162b65ad346bca0402eaffda1d389
María Alejandra G <alis990@hotmail.com>
2019-04-16 19:52:56 -0500

    API implementation to EventsController and CandidatesController

---
+++ XelectionVote.Web/Controllers/API/CandidatesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XelectionVote.Web.Data;
+
+namespace XelectionVote.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class CandidatesController : Controller
+    {
+        private readonly ICandidateRepository candidateRepository;
+
+        public CandidatesController(ICandidateRepository candidateRepository)
+        {
+            this.candidateRepository = candidateRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetCandidates()
+        {
+            return Ok(this.candidateRepository.GetAll());
+        }
+    }
+}
---
+++ XelectionVote.Web/Controllers/API/EventsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XelectionVote.Web.Data;
+
+namespace XelectionVote.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class EventsController : Controller
+    {
+        private readonly IEventRepository eventRepository;
+
+        public EventsController(IEventRepository eventRepository )
+        {
+            this.eventRepository = eventRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetEvents()
+        {
+            return Ok(this.eventRepository.GetAll());
+        }
+    }
+}
